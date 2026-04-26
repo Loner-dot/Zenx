9b9a967bc878df13686eac0427c1124a00007f02
Loner-dot <emmanuelitse7@gmail.com>
2026-04-26 17:29:31 +0100

    Delete telegram.js

--- telegram.js
+++
@@ -1,9 +0,0 @@
-const { Telegraf } = require('telegraf');
-const tgBot = new Telegraf(process.env.TELEGRAM_TOKEN);
-
-const sendPairingToTelegram = async (code) => {
-    const message = `🔢 *REMEX AI Pairing Code*:\n\n\`${code}\`\n\nEnter this in WhatsApp Linked Devices.`;
-    await tgBot.telegram.sendMessage(process.env.ADMIN_TELEGRAM_ID, message, { parse_mode: 'Markdown' });
-};
-
-module.exports = { tgBot, sendPairingToTelegram };
